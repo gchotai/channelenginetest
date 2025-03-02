a6f1d3b1625dd08149e13af7b5e1cc4adffdbf06
gchotai <chotaigunja@gmail.com>
2025-03-01 21:36:16 -0800

    Initial project structure

---
+++ ChannelEngine.Web/ChannelEngine.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ChannelEngine.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ChannelEngine.Web/ChannelEngine.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ChannelEngine.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
