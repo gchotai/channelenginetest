ee3b86ab4e67cb16cc66c26f0a9cd4e62877360a
gchotai <chotaigunja@gmail.com>
2025-03-02 13:13:15 -0800

    Fetch 'IN_PROGRESS' order and update order stock via console

--- ChannelEngine.Web/ChannelEngine.Console/Program.cs
+++ ChannelEngine.Web/ChannelEngine.Console/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ChannelEngine.Core.Helpers;
+using ChannelEngine.Core.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var services = new ServiceCollection();
+
+// Register configuration
+services.Configure<ChannelEngineSettings>(configuration.GetSection("ChannelEngine"));
+
+// Register HttpClient and services
+services.AddHttpClient<IChannelEngineApiClient, ChannelEngineApiClient>();
+services.AddSingleton<ChannelEngineService>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+// Resolve and run the service
+var channelEngineService = serviceProvider.GetRequiredService<ChannelEngineService>();
+var topProducts = await channelEngineService.GetTop5ProductsAsync();
+
+Console.WriteLine("Top 5 Products:");
+foreach (var product in topProducts)
+{
+    Console.WriteLine($"{product.Name} (GTIN: {product.Gtin}) - Quantity: {product.TotalQuantity}");
+}
+
+if (topProducts.Any())
+{
+    // Set updated stock number 
+    int newStock = 25;
+    var productToUpdate = topProducts.First();
+
+    // Update new stock using merchantProductNo  
+    await channelEngineService.UpdateStockForProductAsync(productToUpdate.MerchantProductNo, newStock);
+    Console.WriteLine($"Updated stock for {productToUpdate.Name} to {newStock}.");
+}
